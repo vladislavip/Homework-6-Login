626619ab219048da330769278f2b3d7abf8401c0
vladyslavip <vladyslavip@code.edu.az>
2021-12-11 15:39:13 +0300

    first commit

---
+++ Homework 6 Login/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_6_Login
+{
+    internal class Class1
+    {
+
+        private string username;    //or string username
+        private string password;
+        
+        public string Username
+
+        {
+            get { return username; }      // read
+
+            set { if(value.Length < 5)
+                {
+                    throw new Exception("5 letters minimum");  // write
+                } 
+ 
+                username = value; 
+            
+            }   
+
+
+        }
+        public string Password
+
+        {
+            get { return password; }
+            set
+            {
+                if (value.Length < 8)
+                {
+
+                    throw new Exception("8 symbols minimu");
+
+                }
+
+                password = value;
+            }
+        }
+
+
+        public   bool CheckLoginAndPassword()
+
+        {
+            
+            if (username=="Vlad123" && password=="12345678")
+
+            {
+                Console.WriteLine($"Welcome {username} ");
+                return true;
+            }
+           else
+            {
+
+                Console.WriteLine("Wrong credentials");
+                return false;   
+            }
+        }
+        
+     }
+
+    
+}
---
+++ Homework 6 Login/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Homework_6_Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 a = new Class1();
+
+            step1:
+            Console.WriteLine("Type your new username");
+            a.Username = Console.ReadLine();
+
+            Console.WriteLine("Type your new password");
+            a.Password = Console.ReadLine();
+
+           
+
+            bool b= a.CheckLoginAndPassword();
+
+            if (b==false)
+
+            { goto step1; }
+
+
+
+
+
+
+
+        }
+
+
+
+
+        
+    }
+}
